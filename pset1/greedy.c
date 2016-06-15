15e1cbd17ded08b91d914e6479f927597b1508f9
Richard Davies <rd.codex@live.co.uk>
2016-06-15 17:41:12 +0100

    Update greedy.c

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -6,68 +6,71 @@
 
 int main(void)
 {
-  float change;
-  int cents, coins, quaters, dimes, nickels, pennies;
+	float change;
+	int cents, coins, quaters, dimes, nickels, pennies;
 
-  // Must take positive number input.
-  do
-  {
-    printf("Enter how much change is owed: ");
-    change = GetFloat();
+	// Must take positive number input.
+	do
+	{
+		printf("Enter how much change is owed: ");
+        change = GetFloat();
+		
+	}while(change < 0);
 
-  }while(change < 0);
+	// Turns change into cents.
+	cents = (int) round(change * 100);
 
-  // Turns change into cents.
-  cents = (int) round(change * 100);
+	// instantiate coins.
+	coins = 0;
 
-  // instantiate coins.
-  coins = 0;
+	// instantiate quaters.
+	quaters = 0;
 
-  // instantiate quaters.
-  quaters = 0;
-  
-  // Loop, modulo - Quaters.  
-  while(quaters < cents)
-  {
-    coins += cents / 25;
-    cents %= 25;
-    quaters += 25;
-  }
+	// Loop, modulo - Quaters.
+	while(quaters < cents)
+	{
+		coins += cents / 25;
+        cents %= 25;
+        quaters += 25;
+  	}
 
-  // instantiate dimes.
-  dimes = 0;
-  
-  // Loop, modulo - Dimes.
-  while(dimes < cents)
-  {
-    coins += cents / 10;
-    cents %= 10;
-    dimes += 10;
-  }
+  	// instantiate dimes.
+  	dimes = 0;
 
-  // instantiate nickels.
-  nickels = 0;
-  
-  // Loop, modulo - Nickels.
-  while(nickels < cents)
-  {
-    coins += cents / 5;
-    cents %= 5;
-    nickels += 5;
-  }
+  	// Loop, modulo - Dimes.
+  	while(dimes < cents)
+  	{
+    	coins += cents / 10;
+    	cents %= 10;
+    	dimes += 10;
+  	}
 
-  // instantiate pennies.
-  pennies = 0;
-  
-  // Loop, modulo - Pennies.
-  while(pennies < cents)
-  {
-    coins += cents / 1;
-    pennies += 1;
-  }
+  	// instantiate nickels.
+  	nickels = 0;
 
-  // print result.
-  printf("%i\n", coins);
+  	// Loop, modulo - Nickels.
+  	while(nickels < cents)
+  	{
+    	coins += cents / 5;
+    	cents %= 5;
+    	nickels += 5;
+  	}
 
-  return 0;
+  	// instantiate pennies.
+  	pennies = 0;
+
+  	// Loop, modulo - Pennies.
+  	while(pennies < cents)
+  	{
+    	coins += cents / 1;
+    	pennies += 1;
+  	}
+
+  	// print result.
+  	printf("%i\n", coins);
+
+  	return 0;
 }
+
+
+
