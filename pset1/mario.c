f9e8ad6e9adbd83492346051d1fe492ccf9b796f
Richard Davies <rd.codex@live.co.uk>
2016-06-15 17:27:45 +0100

    Create mario.c

---
+++ pset1/mario.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int height;
+
+	printf("Enter a height value between (0 & 23)\n");
+
+	do
+	{
+		height = GetInt();
+		if(height < 0 || height > 23)
+			printf("Retry: ");
+	}
+	while(height < 0 || height >  23);
+
+	// Loops depending on user input.
+	for(int i = 0; i < (height + 1); i++)
+	{
+		// Prints spaces depending on input.
+		for(int j = 0; j < (height - i); j++ )
+			printf(" ");
+
+		// Prints hashes depending on user input.
+		for(int h = 0; h < i; h++)
+			printf("#");
+
+		// New line with every loop.
+		printf("\n");
+	}
+
+	return 0;
+}
